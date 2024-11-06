416dc2bcdb5818402b63f7bc3eb0e46d20d581aa
zoheb879 <165878411+zoheb879@users.noreply.github.com>
2024-11-06 14:54:34 +0530

    Add files via upload
    add an oppitional

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$(".hover").mouseleave(
+    function() {
+      $(this).removeClass("hover");
+    }
+  );
